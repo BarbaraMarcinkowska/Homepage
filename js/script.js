196e51c22d682f022e65f63eef3017ba3533287d
Barbara Marcinkowska <kropisz_barbara@op.pl>
2023-11-08 11:22:43 +0100

    new function

--- js/script.js
+++ js/script.js
@@ -1,21 +1,45 @@
-function welcome () {
+{ 
+const welcome = () => {
   console.log("Cześć to ja Basia probuje coś działać");
 }
 
-welcome(); 
+const toggleBackground = () => {
+  const body = document.querySelector(".body");
+  const nextColor = document.querySelector(".nextColor");
 
-let button = document.querySelector(".button");
-let body = document.querySelector(".body");
-let nextColor = document.querySelector(".nextColor");
-
-button.addEventListener("click", () => {
   body.classList.toggle ("inerBody");
-
   nextColor.innerText = body.classList.contains("inerBody") ? "ciemny" : "jasny";
-});
+};
 
+const init = () => { 
+  const button = document.querySelector(".button");
+  button.addEventListener("click", toggleBackground);
+  
+  welcome();
+}; 
+
+init(); 
+} 
+ 
+
+//() => {
+  //body.classList.toggle ("inerBody");
+
+ // nextColor.innerText = body.classList.contains("inerBody") ? "ciemny" : "jasny";
+//});
+
+//
   // if (body.classList.contains("body")) {
     //    nextColor.inerText = "jasny"; 
   //  } else { 
   //      nextColor.inerText = "ciemny";
   //  });
+
+  //function welcome () {
+  //  console.log("Cześć to ja Basia probuje coś działać");
+ // }
+
+ // let onChangeBackgrundClick = () => {
+  // body.classList.toggle ("inerBody");
+  // nextColor.innerText = body.classList.contains("inerBody") ? "ciemny" : "jasny";
+//};
